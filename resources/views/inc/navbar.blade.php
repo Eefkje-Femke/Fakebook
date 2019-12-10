a9b9d3f35377aed76d437d5ec02702bacff12733
Eefkje-Femke <123144@student.drenthecollege.nl>
2019-12-10 09:21:40 +0100

    Navbar change

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -14,7 +14,7 @@
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="/posts">Posts</a>
-              </li>
+            </li>
           </ul>
         </div>
 </nav>
