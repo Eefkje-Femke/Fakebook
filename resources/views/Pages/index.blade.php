0bf53712b529e754e28d5d921e31fd0ef0a487b3
AnoukPannenkoek <estheranouk123@gmail.com>
2019-12-10 09:07:36 +0100

    hoi

--- resources/views/Pages/index.blade.php
+++ resources/views/Pages/index.blade.php
@@ -1,11 +1,21 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="jumbotron text-center">
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>{{config('app.name', 'Fakebook')}}</title>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        @include('inc.navbar')
         <div class="container">
-            <h1>{{$title}}</h1>
-            <p>Project Fakebook</p>
-            <p><a class="btn btn-primary btn-lg" href="/login" role="button">log in</a> <a class="btn btn-success btn-lg" href="/register" role="button">register</a></p>
+            <div class="jumbotron text-center">
+                <div class="container">
+                    <h1>{{$title}}</h1>
+                    <p>Project Fakebook</p>
+                    <p><a class="btn btn-primary btn-lg" href="/login" role="button">log in</a> <a class="btn btn-success btn-lg" href="/register" role="button">register</a></p>
+                </div>
+            </div>
         </div>
-    </div>
-@endsection
+    </body>
+</html>
