a5694cfc6ce88af203d4d5abc0c9ed85e6c6a180
AnoukPannenkoek <estheranouk123@gmail.com>
2019-12-11 10:51:13 +0100

    er is nu een login en register pagina

--- resources/views/login/index.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <h1>Login</h1>
-    <p>Project Fakebook</p>
-    <p>Hoi</p>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,10 @@ Route::get('/about', 'PagesController@about');
 // Route::get('/about', function () {
 //     return view('pages.about');
 // });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
