ce9aade495624966fd1f93bf49cb34cde4eac0ea
AnoukPannenkoek <estheranouk123@gmail.com>
2019-12-12 09:25:43 +0100

    Navbar aangepast

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,6 @@ Route::get('/about', 'PagesController@about');
 // Route::get('/about', function () {
 //     return view('pages.about');
 // });
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
