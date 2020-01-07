593c929f2b3f705fff0e46260fae349d9fe71296
Eefkje-Femke <123144@student.drenthecollege.nl>
2020-01-07 11:43:30 +0100

    navbar change

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -16,10 +16,7 @@
         </ul> --}}
 
         <!-- Right Side Of Navbar -->
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="{{ action('PostsController@create')}}">Create post</a>
-          </li>
+        <ul class="navbar-nav ml-auto">          
             <!-- Authentication Links -->
             @guest
                 <li class="nav-item">
@@ -31,6 +28,9 @@
                     </li>
                 @endif
             @else
+              <li class="nav-item">
+                <a class="nav-link" href="{{ action('PostsController@create')}}">Create post</a>
+              </li>
                 <li class="nav-item dropdown">
                     <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                         {{ Auth::user()->name }} <span class="caret"></span>
