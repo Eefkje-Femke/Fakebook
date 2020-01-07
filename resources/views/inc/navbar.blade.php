4fa598fddb41acf56f5cb150da634b1ff61769bc
Eefkje-Femke <123144@student.drenthecollege.nl>
2020-01-07 09:40:11 +0100

    create post added navbar

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,28 +1,3 @@
-{{--<nav class="navbar navbar-expand-md navbar-dark bg-dark">
-        <a class="navbar-brand" href="/">{{config('app.name', 'Fakebook')}}</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-      
-        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item">
-              <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="/about">About</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="/posts">Posts</a>
-            </li>
-          </ul>
-          <ul class='nav navbar-nav navbar-right'>
-            <li><a class="nav-link" href="/posts/create">Create Post</a></li>
-          </ul>
-        </div>
-</nav>--}}
-
-
 <nav class="navbar navbar-expand-md navbar-dark" style="background-color: #4267B2;">
   <div class="container">
     <a class="navbar-brand" href="{{ url('/') }}"> 
@@ -51,6 +26,9 @@
                     </li>
                 @endif
             @else
+                <li>
+                  <a href="{{ action('PostsController@create')}}" style="margin:10px;">Create post</a>
+                </li>
                 <li class="nav-item dropdown">
                     <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                         {{ Auth::user()->name }} <span class="caret"></span>
@@ -59,10 +37,7 @@
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                       <a class="dropdown-item">
                         Profiel
-                      </a> 
-                      <a class="dropdown-item">
-                        Create post
-                      </a> 
+                      </a>
                       <a class="dropdown-item">
                         Instellingen
                       </a> 
