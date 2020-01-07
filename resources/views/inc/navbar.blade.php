445e0f003c739dc728287e4d2d1975b706fe7f19
Eefkje-Femke <123144@student.drenthecollege.nl>
2020-01-07 08:57:36 +0100

    check of alles is gecommit

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -23,7 +23,6 @@
 </nav>--}}
 
 
-<nav class="navbar navbar-expand-md navbar-dark bg-dark">
 <nav class="navbar navbar-expand-md navbar-dark" style="background-color: #4267B2;">
   <div class="container">
     <a class="navbar-brand" href="{{ url('/') }}"> 
