8f23f81cfc9186b4177d658cf985bc208f023041
AnoukPannenkoek <estheranouk123@gmail.com>
2020-01-09 11:36:34 +0100

    Table aangemaakt voor gegevens in profile

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class ProfileController extends Controller
 {
     public function index(){
-        return view('profile.profile');
+
+        $profiles = User::all()->toArray();
+        return view('profile.profile', compact('profiles'));
     }
 }
--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -2,7 +2,19 @@
 
 @section('content')
     <h1>{{ Auth::user()->name}} </h1>
-    {{--@foreach($iterable as $key)
-        
-    @endforeach--}}
+    <table class="table">
+        <tr>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Email</th>
+        </tr>
+        @foreach($profiles as $profile)
+        <tr>
+            <td>{{$profile['id']}}</td>
+            <td>{{$profile['name']}}</td>
+            <td>{{$profile['email']}}</td>
+        </tr>
+        @endforeach
+    </table>
+   
 @endsection
