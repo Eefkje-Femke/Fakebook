03b1282e1a2d96cde56bdb2ae32e7656df218390
Eefkje-Femke <123144@student.drenthecollege.nl>
2020-01-24 11:11:48 +0100

    home controller ook verbeterd

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,9 +23,5 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
-    {
-        $users =  User::where('id', '!=', auth()->id())->simplePaginate(5);
-        return view('home', compact('users'));
-    }
+
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,31 +14,8 @@
                         @endif
                         <h1>Welcome {{Auth::user()->name}}!</h1>
                         You are logged in!
-                    </div>                    
-                    {{-- <div>
-                        <h3 style="margin-left:15px;">Lijst gebruikers</h3>
-                        <table class="table">
-                            <tr>
-                                <th>Name</th>
-                                <th>Email</th>
-                                <th>Follow</th>
-                                <th>Unfollow</th>
-                            </tr>                            
-                                @foreach($users as $user)   
-                                    <tr>                         
-                                        <td>{{$user->name}}</td>
-                                        <td>{{$user->email}}</td>
-                                        <td><a class="btn btn-primary" role="button" href="{{ route('followUser', ['user_id' => $user->id]) }}">
-                                            Follow User
-                                        </a></td>
-                                        <td><a class="btn btn-danger" role="button" href="{{ route('unfollowUser', ['user_id' => $user->id]) }}">
-                                            Unfollow User
-                                        </a></td>
-                                    </tr>                              
-                                @endforeach                            
-                        </table>
-                       {{ $users->links() }}{{--paginatie --}}
-                    </div> --}}
+                    </div>            
+                    </div> 
                 </div>
             </div>
         </div>
